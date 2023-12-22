61e5c1962855eaefb61b1038cd2ff24007bfe3e0
isaacglezacosta <isaacglezacosta10@gmail.com>
2023-12-22 13:44:23 -0600

    Nuevo carácter encontrado

--- Cifrado César.py
+++ Cifrado César.py
@@ -2,6 +2,8 @@ text = "Hello World"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-for char in text:
+for char in text.lower():
     index = alphabet.find(char)
-    print(char, index)
+    new_index = index + shift
+    new_char = alphabet[new_index]
+    print("char:", char, "new char:", new_char)
