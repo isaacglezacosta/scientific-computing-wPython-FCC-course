003e598cb98e2153d8f4d7f06a67b58eb8359c4e
isaacglezacosta <isaacglezacosta10@gmail.com>
2023-12-22 13:21:09 -0600

    Loop para encontrar la posición por carácter

--- Cifrado César.py
+++ Cifrado César.py
@@ -1,7 +1,7 @@
 text = "Hello World"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-index = alphabet.find(text[0].lower())
-print(index)
-shifted = alphabet[index + shift]
-print(shifted)
+
+for char in text:
+    index = alphabet.find(char)
+    print(char, index)
