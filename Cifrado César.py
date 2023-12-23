bdb8b36542c9a07f7b693228c8307a8b013b262c
isaacglezacosta <isaacglezacosta10@gmail.com>
2023-12-22 18:26:40 -0600

    Los espacios se toman como espacios

--- Cifrado César.py
+++ Cifrado César.py
@@ -1,9 +1,13 @@
 text = "Hello World"
 shift = 3
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+encrypted_text = ""
 
 for char in text.lower():
-    index = alphabet.find(char)
-    new_index = index + shift
-    new_char = alphabet[new_index]
-    print("char:", char, "new char:", new_char)
+    if char == " ":
+        encrypted_text += char
+    else:
+        index = alphabet.find(char)
+        new_index = index + shift
+        encrypted_text += alphabet[new_index]
+        print("char:", char, "encrypted text:", encrypted_text)
